41ee24d200647c0a5ba32c37e51a11eba755703f
rgan <rajaram.ganeshan@gmail.com>
2012-05-30 13:39:06 -0400

    Handle course not found

--- controllers/course_controller.rb
+++ controllers/course_controller.rb
@@ -1,11 +1,11 @@
-before do
-  content_type 'application/json'
-end
-
 get "/courses" do
+  content_type 'application/json'
   Course.all.to_json
 end
 
 get "/courses/:id/enrolments" do
-  Course.get(params[:id].to_i).enrolments.to_json
+  course = Course.get(params[:id].to_i)
+  halt(404, 'Not Found') if course.nil?
+  content_type 'application/json'
+  course.enrolments.to_json
 end
--- spec/controllers/course_controller_spec.rb
+++ spec/controllers/course_controller_spec.rb
@@ -7,7 +7,7 @@ describe "course controller" do
     Sinatra::Application
   end
 
-  it "/courses" do
+  it "GET /courses" do
     course = FactoryGirl.build(:course)
     Course.should_receive(:all).and_return([course])
     get "/courses"
@@ -16,12 +16,26 @@ describe "course controller" do
     last_response.body.should == [course].to_json
   end
 
-  it "/courses/:id/enrolments" do
-    course = FactoryGirl.build(:course_with_enrolments)
-    Course.should_receive(:get).with(1).and_return(course)
-    get "/courses/1/enrolments"
-    last_response.status.should == 200
-    last_response.headers["Content-Type"].should == "application/json;charset=utf-8"
-    last_response.body.should == course.enrolments.to_json
+  describe "GET /courses/:id/enrolments" do
+    it "should return 404 not found" do
+      Course.should_receive(:get).and_return(nil)
+      get "/courses/1/enrolments"
+      last_response.status.should == 404
+    end
+
+    it "should return enrolments" do
+      course = FactoryGirl.build(:course_with_enrolments)
+      Course.should_receive(:get).with(1).and_return(course)
+      get "/courses/1/enrolments"
+      last_response.status.should == 200
+      last_response.headers["Content-Type"].should == "application/json;charset=utf-8"
+      last_response.body.should == course.enrolments.to_json
+    end
+  end
+
+  describe "POST /courses" do
+    it "should return error if data is not valid" do
+
+    end
   end
 end
